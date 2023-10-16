19a6494b9547eaf008a4574236865fee9e7b3944
Tyrussgc <141864114+Tyrussgc@users.noreply.github.com>
2023-10-16 02:38:50 -0500

    -_-

--- Deportista.py
+++ Deportista.py
@@ -1,16 +1,13 @@
-class Deportista():
-    def __init__(self,añosPracticando):
+class Deportista:
+    def __init__(self, añosPracticando):
         self._deporte = "Futbol"
         self._añosPracticando = añosPracticando
 
     def getDeporte(self):
         return self._deporte
-    
-    def setDeporte(self,Deporte):
-        self._deporte = Deporte
-
+    def setDeporte(self, deporte):
+        self._deporte = deporte
     def getAñosPracticando(self):
         return self._añosPracticando
-    
-    def setAñosPracticando(self,AñosPracticando):
-        self._añosPracticando = AñosPracticando
+    def setAñosPracticados(self, añosPracticando):
+        self._añosPracticando = añosPracticando
